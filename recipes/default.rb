9931657e2e6df33ba39dfe2fc5b0053e84f0db62
Michael Avila <me@michaelavila.com>
2015-12-12 14:58:37 -0800

    Add fast first implementation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,33 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'apt::default'
+
+apt_repository 'hockeypuck' do
+  uri 'ppa:hockeypuck/unstable'
+  distribution 'trusty'
+end
+
+package 'hockeypuck' do
+  action :install
+end
+
+file '/etc/default/hockeypuck' do
+  content 'initstart=yes'
+end
+
+cookbook_file '/etc/hockeypuck/hockeypuck.conf' do
+  source 'hockeypuck.conf'
+end
+
+directory '/var/log/hockeypuck' do
+  owner node['hockeypuck']['user']
+  group node['hockeypuck']['group']
+end
+
+execute 'hockeypuck -config /etc/hockeypuck/hockeypuck.conf &' do
+  cwd   node['hockeypuck']['working_dir']
+  user  node['hockeypuck']['user']
+  group node['hockeypuck']['group']
+end
