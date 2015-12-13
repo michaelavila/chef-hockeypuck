cd9c4cad0ababbedc0b801f6d07c292fd6fe32c6
Michael Avila <me@michaelavila.com>
2015-12-12 17:39:21 -0800

    Add missing dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'me@michaelavila.com'
 license          'GNU Affero General Public License'
 description      'Installs/Configures hockeypuck'
 long_description 'Installs/Configures hockeypuck'
-version          '0.1.2'
+version          '0.1.3'
+
+depends 'apt'
